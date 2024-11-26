3c9a6ad8a54c6f3e55c548c2464d85a28efda5aa
Yelyn Ahn <yelynahn@gmail.com>
2024-11-26 05:19:41 +0000

    feat: Add file_name params

--- logos_pipe_ocr/util/file.py
+++ logos_pipe_ocr/util/file.py
@@ -3,8 +3,8 @@ import tempfile
 import os
 from pathlib import Path
 
-def create_text_file(text: str, file_path: str) -> None: # 텍스트 파일을 저장하는 함수
-    with open(file_path, 'w', encoding='utf-8') as file:
+def create_text_file(text: str, file_path: str, file_name: str) -> None: # 텍스트 파일을 저장하는 함수
+    with open(str(file_path) + '/' + file_name, 'w', encoding='utf-8') as file:
         file.write(text)    
 
 def read_json_file(file_path: str) -> dict: # JSON 파일을 읽어오는 함수
@@ -18,9 +18,9 @@ def read_json_file(file_path: str) -> dict: # JSON 파일을 읽어오는 함수
         print(f"Error occurred: {e}")
         return None
 
-def create_json_file(data: dict, file_path: str) -> None: # JSON 파일을 저장하는 함수
+def create_json_file(data: dict, file_path: str, file_name: str) -> None: # JSON 파일을 저장하는 함수
     try:
-        with open(file_path + '.json', 'w', encoding='utf-8-sig') as file:
+        with open(str(file_path) + '/' + file_name + '.json', 'w', encoding='utf-8-sig') as file:
             json.dump(data, file, ensure_ascii=False, indent=4)
     except Exception as e:
         raise Exception(f"An error occurred while creating a JSON file: {str(e)}")
@@ -30,7 +30,7 @@ def increment_path(path, exist_ok=False, sep="", mkdir=False):
     Generates an incremented file or directory path if it exists, with optional mkdir; args: path, exist_ok=False,
     sep="", mkdir=False.
 
-    Example: runs/exp --> runs/exp_{sep}2, runs/exp_{sep}3, ... etc
+    Example: runs/exp --> runs/exp_{sep}1, runs/exp_{sep}2, ... etc
     """
     path = Path(path)  # os-agnostic
     if path.exists() and not exist_ok:
