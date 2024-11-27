282dc2495db2c743dd9d42f420117f2613ebef0c
Yelyn Ahn <yelynahn@gmail.com>
2024-11-27 01:42:49 +0000

    feat: Add instance and get_func

--- logos_pipe_ocr/eval/schema_generator.py
+++ logos_pipe_ocr/eval/schema_generator.py
@@ -3,7 +3,7 @@ This module generates a JSON schema from a JSON file.
 """
 from abc import ABC, abstractmethod
 
-class generator(ABC):
+class SchemaGenerator(ABC):
     """Abstract class for generating JSON schemas."""
     @abstractmethod
     def generate_json_schema(self) -> dict:
@@ -11,42 +11,42 @@ class generator(ABC):
         pass
 
     @abstractmethod
-    def process_data(self, schema: dict):
+    def _process_data(self, schema: dict):
         """Process data into the schema."""
         pass
 
-class JsonSchemaGenerator(generator):
-    """Generates a JSON schema from provided JSON data."""
-    def __init__(self, json_data: dict):
-        """Initialize with JSON data."""
-        self.json_data = json_data
+class JsonSchemaGenerator(SchemaGenerator):
+    """generate json schema, required fields, boolean fields from json data"""
+    def __init__(self, json_data: dict = None):
+        self.json_data = json_data if json_data is not None else {}
+        self.schema = {"type": "object", "properties": {}, "required": [], "additionalProperties": False}
+        self.required_fields = []
+        self.boolean_fields = {}
+        self.generate_json_schema()
 
     def generate_json_schema(self) -> dict:
         """Generate the JSON schema."""
-        schema = {"type": "object", "properties": {}, "required": [], "additionalProperties": False}
         if self.json_data:
-            self.process_data(schema)
-        return schema
-
-    def process_data(self, schema: dict):
-        """Process JSON data into the schema."""
-        if isinstance(self.json_data, list):
-            for key, value in self.json_data[0].items():
-                schema["properties"][key] = self.get_type(value)
-                schema["required"].append(key)
+            self._process_data(self.schema)
+            self.required_fields = self.get_required_fields()
+            self.boolean_fields = self.get_boolean_fields()
         else:
-            for key, value in self.json_data.items():
-                schema["properties"][key] = self.get_type(value)
-                schema["required"].append(key)
+            raise ValueError("JSON data is empty")
 
-    def get_type(self, value: dict | list | str) -> dict:
+    def _process_data(self, schema: dict):
+        """Process JSON data into the schema."""
+        data_items = self.json_data[0] if isinstance(self.json_data, list) else self.json_data
+        for key, value in data_items.items():
+            schema["properties"][key] = self._get_type(value)
+            schema["required"].append(key)
+
+    def _get_type(self, value: dict | list | str | None) -> dict:
         """Return the type of the given value."""
         if isinstance(value, dict):
-            return {"type": "object", "properties": JsonSchemaGenerator(value).generate_json_schema()["properties"]}
+            return {"type": "object", "properties": JsonSchemaGenerator(value).get_properties()}
         elif isinstance(value, list):
-            return {"type": "array", "items": self.get_type(value[0]) if value else self.get_type(None)}
+            return {"type": "array", "items": self._get_type(value[0]) if value else self._get_type(None)}
         else:
-            # 모든 타입을 JSON 스키마에 맞는 문자열로 변환
             type_mapping = {
                 int: "integer",
                 float: "number",
@@ -55,3 +55,18 @@ class JsonSchemaGenerator(generator):
                 type(None): "string" # if value is None, type is string
             }
             return {"type": type_mapping.get(type(value), "string")}
+        
+    def get_properties(self) -> dict:
+        return self.schema["properties"]
+
+    def get_required_fields(self) -> list:
+        """Return the required fields."""
+        return self.schema["required"]
+
+    def get_boolean_fields(self) -> dict:
+        """Return the boolean fields."""
+        return {key: value for key, value in self.schema["properties"].items() if value["type"] == "boolean"}
+
+    def get_schema(self) -> dict:
+        """Return the schema."""
+        return self.schema
