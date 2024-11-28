e62f86079d87f78b9e8e40133c6ed4cad27707ba
Yelyn Ahn <yelynahn@gmail.com>
2024-11-28 02:19:24 +0000

    feat: Add EvalDataHandler

--- logos_pipe_ocr/util/datahandlers.py
+++ logos_pipe_ocr/util/datahandlers.py
@@ -6,6 +6,8 @@ from abc import ABC, abstractmethod
 import base64
 import json
 import PIL.Image
+from logos_pipe_ocr.util.dataloaders import EvalDataLoader
+from logos_pipe_ocr.util.file import read_json_file, read_txt_file
 
 class ImageProcessor(ABC):
     @abstractmethod
@@ -63,9 +65,8 @@ class ChatGPTResponseHandler(ResponseHandler):
         """
         if response.choices[0].message.content is not None:
             response_json = json.loads(response.choices[0].message.content)
-            response_dict = response_json.get("questions", response_json)
-            self.add_file_name(response_dict, image_file_path)
-            return response_dict
+            self.add_file_name(response_json, image_file_path)
+            return response_json
         return {}
 
 class GeminiResponseHandler(ResponseHandler):
@@ -81,7 +82,73 @@ class GeminiResponseHandler(ResponseHandler):
         """
         if response.candidates:
             response_json = json.loads(response.text)
-            response_dict = response_json.get("questions", response_json)
-            self.add_file_name(response_dict, image_file_path)
-            return response_dict
+            self.add_file_name(response_json, image_file_path)
+            return response_json
         return {}
+
+
+class EvalDataHandler(EvalDataLoader):
+    """ Handle the evaluation data by inheriting from EvalDataLoader.
+
+    Args:
+        label_dir_path (str): The path to the label directory.
+        output_dir_path (str): The path to the output directory.
+    
+    Returns:
+        eval_data (dict): A dictionary containing the processed data.  
+    """
+    def __init__(self, label_dir_path: str, output_dir_path: str):
+        super().__init__(label_dir_path, output_dir_path)
+
+    def __call__(self) -> dict:
+        label_file_paths = self.get_label_file_paths()
+        output_file_paths = self.get_output_file_paths()
+        self._current_index = 0
+        
+        processed_data = []
+        self.eval_data = {}
+
+        for label, output in zip(label_file_paths, output_file_paths):
+            if label.endswith(".json") and output.endswith(".json"):
+                processed_item = {
+                    "label": read_json_file(label),
+                    "output": read_json_file(output),
+                }
+            elif label.endswith(".txt") and output.endswith(".txt"):
+                processed_item = {
+                    "label": read_txt_file(label),
+                    "output": read_txt_file(output),
+                }
+            else:
+                raise ValueError(f"Unsupported file extension: {label} or {output}")
+            
+            processed_data.append(processed_item)
+
+        self.eval_data["processed_data"] = processed_data
+        return self.eval_data
+    
+    def __len__(self) -> int:
+        return len(self.eval_data["processed_data"])
+    
+    def __getitem__(self, index: int) -> dict:
+        return self.eval_data["processed_data"][index]
+    
+    def __iter__(self):
+        for item in self.eval_data.get("processed_data", []):
+            yield item["label"], item["output"]
+    
+    def __next__(self):
+        if self._current_index >= len(self.eval_data["processed_data"]):
+            raise StopIteration
+        item = self.eval_data["processed_data"][self._current_index]
+        self._current_index += 1
+        return item
+
+    def get_eval_data(self) -> dict:
+        return self.eval_data
+
+    def get_label_data(self):
+        return [item["label"] for item in self.eval_data["processed_data"]]
+
+    def get_output_data(self):
+        return [item["output"] for item in self.eval_data["processed_data"]]
