18df058e21ec232100b4d348de49e6f16d68fe3f
Yelyn Ahn <yelynahn@gmail.com>
2024-11-29 09:07:46 +0000

    refactor: Implement template method pattern

--- logos_pipe_ocr/val/validation.py
+++ logos_pipe_ocr/val/validation.py
@@ -26,8 +26,9 @@ class Validation(Validator):
     """
     def __init__(self, eval_metrics: str) -> None:
         super().__init__()
+        self.eval_metrics = eval_metrics
         self.fidelity_validator = FidelityValidation()
-        self.text_validator = TextValidation(eval_metrics)
+        self.text_validator = TextValidation()
         self.validation_schema = None
         self.required_fields = None
         self.boolean_fields = None
@@ -44,17 +45,21 @@ class Validation(Validator):
             result_str += f" Text Validation Results: {result['text_validation_results']}\n"
             result_str += f"--------S--------------------------\n"
         return result_str
+    
+    def save(self, save_path: str, save_format: str) -> None:
+        save(self.validation_results, save_path, save_format)   
 
     def run(self, file_name: str, processed_predicted_data: list[dict] | dict, processed_ground_truth_data: list[dict] | dict) -> dict: 
         self.file_name = file_name
-
-        self.fidelity_validator.run(processed_predicted_data, processed_ground_truth_data)
-        self.text_validator.run(processed_predicted_data, processed_ground_truth_data)
-
+        self._run_validators(processed_predicted_data, processed_ground_truth_data)
         self._create_combined_results()
         print(self)
         return self.validation_results
-    
+
+    def _run_validators(self, processed_predicted_data, processed_ground_truth_data) -> None:
+        self.fidelity_validator.run(processed_predicted_data, processed_ground_truth_data)
+        self.text_validator.run(processed_predicted_data, processed_ground_truth_data)
+
     def _get_json_schema(self, processed_ground_truth_data: list[dict] | dict) -> None:
         _generator = JsonSchemaGenerator(processed_ground_truth_data)
         self.validation_schema = _generator.schema
@@ -69,46 +74,51 @@ class Validation(Validator):
                 "text_validation_results": self.text_validator.text_validation_results[i] 
             })
 
-    def save(self, save_path: str, save_format: str) -> None:
-        save(self.validation_results, save_path, save_format)
+    def _validate_data(self, processed_predicted_data, processed_ground_truth_data, validate_function) -> None:
+        if isinstance(processed_ground_truth_data, list):
+            for i, ground_truth_data in enumerate(processed_ground_truth_data):
+                if self._has_predicted_data(i):
+                    predicted_data = processed_predicted_data[i]
+                    validate_function(predicted_data, ground_truth_data)
+                else:
+                    self._handle_missing_data()
+        else:
+            if processed_predicted_data is not None:
+                validate_function(processed_predicted_data, processed_ground_truth_data)
+            else:
+                self._handle_missing_data()
+    
+    def _has_predicted_data(self, index: int) -> bool:
+        return index < len(self.processed_predicted_data) and self.processed_predicted_data[index] is not None
 
 class FidelityValidation(Validation):
+    """ FidelityValidation class for validating the fidelity of the model.
+
+    Attributes:
+        processed_predicted_data (list[dict] | dict): The processed predicted data.
+    
+    Returns:
+        fidelity_validation_results (dict): A dictionary containing the fidelity validation results.
+    """
     def __init__(self) -> None:
         self.processed_predicted_data = None
         self.processed_ground_truth_data = None
         self.fidelity_validation_results = []
 
     def run(self, processed_predicted_data: list[dict] | dict, processed_ground_truth_data: list[dict] | dict) -> dict:
-        if not processed_ground_truth_data:
-            raise ValueError("Ground truth data is empty. Cannot proceed with validation.")
-        
-        super()._get_json_schema(processed_ground_truth_data)
+        self._check_ground_truth_data(processed_ground_truth_data)
+        self._get_json_schema(processed_ground_truth_data)
         self.processed_predicted_data = processed_predicted_data
         self.processed_ground_truth_data = processed_ground_truth_data
-
         self._validate_prompt_fidelity()
         return self.fidelity_validation_results
 
-    def _validate_prompt_fidelity(self) -> None:            
-        if isinstance(self.processed_ground_truth_data, list):
-            for i, ground_truth_data in enumerate(self.processed_ground_truth_data):
-                if self._has_predicted_data(i):
-                    predicted_data = self.processed_predicted_data[i]
-                    self._validate_single_data_prompt_fidelity(predicted_data, ground_truth_data)
-                else:
-                    self._handle_missing_data()
-        else:
-            if self.processed_predicted_data is not None:
-                self._validate_single_data_prompt_fidelity(self.processed_predicted_data, self.processed_ground_truth_data)
-            else:
-                self._handle_missing_data()
+    def _check_ground_truth_data(self, processed_ground_truth_data) -> None:
+        if not processed_ground_truth_data:
+            raise ValueError("Ground truth data is empty. Cannot proceed with validation.")
 
-    def _has_predicted_data(self, index: int) -> bool:
-        return index < len(self.processed_predicted_data) and self.processed_predicted_data[index] is not None
-
-    def _handle_missing_data(self) -> None:
-        self.fidelity_validation_results.append({"schema_validity": False, "missing_fields": self.required_fields, "boolean_result": None})
-        print("WARNING: There is no generated data compared to the ground truth data.") # TODO: Need to set as exception?
+    def _validate_prompt_fidelity(self) -> None:
+        self._validate_data(self.processed_predicted_data, self.processed_ground_truth_data, self._validate_single_data_prompt_fidelity)
 
     def _validate_single_data_prompt_fidelity(self, predicted_data, ground_truth_data) -> None:
         self.schema_validity, self.missing_fields = validate_json_schema(predicted_data, self.validation_schema)
@@ -118,60 +128,68 @@ class FidelityValidation(Validation):
             "missing_fields": self.missing_fields, 
             "boolean_result": self.boolean_result
         })
+    
+    def _handle_missing_data(self) -> None:
+        # if there is no predicted data(list elements), create a data_valid_dict with False, missing_fields, and None
+        self.fidelity_validation_results.append({"schema_validity": False, "missing_fields": self.required_fields, "boolean_result": None})
+        print("WARNING: There is no generated data compared to the ground truth data.") # TODO: Need to set as exception?
 
 class TextValidation(Validation):
-    def __init__(self, eval_metrics: str) -> None:
-        super().__init__()
-        self.eval_metrics = eval_metrics
+    """ TextValidation class for validating the text detection performance of the model.
 
-    def run(self, processed_predicted_data: list[dict] | dict, processed_ground_truth_data: list[dict] | dict) -> None:
-        try:    
-            self.boolean_fields = None
-            self._validate_text_detection()
-            print("INFO: Text validation is completed.")
-        except Exception as e:
-            return f"Text validation error: {e}"
+    Attributes:
+        processed_predicted_data (list[dict] | dict): The processed predicted data.
+    
+    Returns:
+        text_validation_results (list[dict]): A list of dictionaries containing the text validation results.
+    """
+    def __init__(self) -> None:
+        self.processed_predicted_data = None
+        self.processed_ground_truth_data = None
+        self.text_validation_results = []
+
+    def run(self, processed_predicted_data: list[dict] | dict, processed_ground_truth_data: list[dict] | dict) -> list[dict]:
+        self._check_ground_truth_data(processed_ground_truth_data)
+        self._get_json_schema(processed_ground_truth_data)
+        self.processed_predicted_data = processed_predicted_data
+        self.processed_ground_truth_data = processed_ground_truth_data
+        self._validate_text_detection()
+        return self.text_validation_results
+    
+    def _check_ground_truth_data(self, processed_ground_truth_data) -> None:
+        if not processed_ground_truth_data:
+            raise ValueError("Ground truth data is empty. Cannot proceed with validation.")
 
     def _validate_text_detection(self) -> None:
-        if isinstance(self.processed_ground_truth_data, list):
-            for i, ground_truth_data in enumerate(self.processed_ground_truth_data):
-                if self._has_predicted_data(i):
-                    predicted_data = self.processed_predicted_data[i]
-                    self._validate_single_data_text_detection(predicted_data, ground_truth_data)
-                else:
-                    self._handle_missing_data_text_detection(ground_truth_data)
-        else:
-            if self.processed_predicted_data is not None:
-                self._validate_single_data_text_detection(self.processed_predicted_data, self.processed_ground_truth_data)
-            else:
-                self._handle_missing_data_text_detection(self.processed_ground_truth_data)
-
-    def _handle_missing_data_text_detection(self, ground_truth_data) -> None:
-        self.text_validation_results.append(self._create_data_valid_dict(ground_truth_data))
-        print("WARNING: There is no generated data compared to the ground truth data.")
+        self._validate_data(self.processed_predicted_data, self.processed_ground_truth_data, self._validate_single_data_text_detection)
 
     def _validate_single_data_text_detection(self, predicted_data, ground_truth_data) -> None:  
         data_valid_dict = {}
         for key in ground_truth_data.keys():  
             if key == "file_name":
                 continue
-            elif key not in predicted_data.keys():  
-                data_valid_dict[key] = {"cer": 1.0}
+            elif key not in predicted_data.keys():  # if the key is not in predicted_data, can't calculate metrics
+                print(f"WARNING: Can't calculate metrics. The key '{key}' is not in the predicted data.")
                 continue
 
             # predicted_data와 ground_truth_data의 값이 리스트인 경우
             predicted_text = predicted_data.get(key) if not isinstance(predicted_data.get(key), list) else predicted_data[key]
             ground_truth_text = ground_truth_data.get(key) if not isinstance(ground_truth_data.get(key), list) else ground_truth_data[key]
 
-            evaluation_result = evaluate_text_detection(predicted_text, ground_truth_text)
+            evaluation_result = evaluate_text_detection(predicted_text, ground_truth_text, self.eval_metrics)
             if evaluation_result is not None:  
                 data_valid_dict[key] = evaluation_result
         self.text_validation_results.append(data_valid_dict)  
+
+    def _handle_missing_data(self) -> None: # TODO: Need to set as exception? 
+        # if there is no predicted data(list elements), create a data_valid_dict with error_rate 1.0
+        self.text_validation_results.append(self._create_data_valid_dict(self.processed_ground_truth_data))
+        print("WARNING: There is no generated data compared to the ground truth data.")
 
     def _create_data_valid_dict(self, ground_truth_data) -> dict:
         data_valid_dict = {}
         for key in ground_truth_data.keys():
             if key != "file_name" and key not in self.boolean_fields:
-                data_valid_dict[key] = {"cer": 1.0}
+                data_valid_dict[key] = {metric: 0.0 if metric == "accuracy" else 1.0 for metric in self.eval_metrics}
         return data_valid_dict
     
