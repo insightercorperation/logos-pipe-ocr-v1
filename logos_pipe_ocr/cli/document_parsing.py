f501e89f0ada0b9dee54d9086cb0f85ac369ceda
Yelyn Ahn <yelynahn@gmail.com>
2024-12-03 08:20:04 +0000

    style: Add discussion

--- logos_pipe_ocr/cli/document_parsing.py
+++ logos_pipe_ocr/cli/document_parsing.py
@@ -11,7 +11,7 @@ def add_arguments(parser: argparse.ArgumentParser):
 def main(image_path: str, prompt_file_path: str, model_name: str, model_config_path: str):
     # load the model and run the model
     model = load_model(model_name, model_config_path)
-    model.run(prompt_file_path, image_path) # TODO: Need to move prompt_file_path to the load_model function?
+    model.run(prompt_file_path, image_path) # TODO: Need to move prompt_file_path to the load_model function? 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="logos-pipe-ocr: Document Parsing CLI")
